72f1577596855a5ddd7d3eefbc8ea62f9cb3db9e
ogabek542 <otaxonovo22222@gmail.com>
2025-11-13 17:27:26 +0500

    added hader section components

--- my-app/app/(home)/components/index.ts
+++ my-app/app/(home)/components/index.ts
@@ -1,4 +1,9 @@
 import { Navbar } from "./navbar";
 import { Logo } from "./logo";
+import { Heroes } from "./heroes";
+import { Clients } from "./clients";
+import { Pricing } from "./pricing";
+import { PricingCard } from "./pricing-card";
+import { Footer } from "./footer";
 
-export { Navbar, Logo };
+export { Navbar, Logo, Heroes, Clients, Pricing, PricingCard, Footer };
