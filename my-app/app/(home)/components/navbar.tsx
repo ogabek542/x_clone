6e76f61e5c5a7709d0b07d10b6f95482d09f9072
ogabek542 <otaxonovo22222@gmail.com>
2025-11-15 13:56:47 +0500

    added authentification but can not work auth

--- my-app/app/(home)/components/navbar.tsx
+++ my-app/app/(home)/components/navbar.tsx
@@ -6,9 +6,15 @@ import { ModeToggle } from "@/components/shared/mode-toggle";
 import { Button } from "@/components/ui/button";
 import { useScrolled } from "@/hooks/use-scrolled";
 import { cn } from "@/lib/utils";
+import { SignInButton, UserButton } from "@clerk/nextjs";
+import { useConvexAuth } from "convex/react";
+import Link from "next/link"; // Fixed: Import Link from next/link instead of lucide-react
+import { Loader } from "@/components/ui/loader";
 
 export const Navbar = () => {
   const scrolled = useScrolled();
+  const { isAuthenticated, isLoading } = useConvexAuth();
+
   return (
     <div
       className={cn(
@@ -17,12 +23,32 @@ export const Navbar = () => {
       )}
     >
       <Logo />
-      <div className="flex items-center gap-x-2 ">
-        <Button size={"sm"} variant={"ghost"}>
-          Log In
-        </Button>
-        <Button size={"sm"}>Get Notion Free</Button>
-        <ModeToggle />
+      <div className="flex items-center gap-x-2">
+        {isLoading && <Loader size={"lg"} />}
+
+        {!isAuthenticated && !isLoading && (
+          <>
+            <SignInButton>
+              <Button size={"sm"} variant={"ghost"}>
+                Log In
+              </Button>
+            </SignInButton>
+            <SignInButton>
+              <Button size={"sm"}>Get Notion Free</Button>
+            </SignInButton>
+            <ModeToggle />
+          </>
+        )}
+
+        {isAuthenticated && !isLoading && (
+          <>
+            <Button size={"sm"} variant={"ghost"} asChild>
+              <Link href="/documents">Enter Notion</Link>
+            </Button>
+            <UserButton afterSignOutUrl="/" />
+            <ModeToggle />
+          </>
+        )}
       </div>
     </div>
   );
