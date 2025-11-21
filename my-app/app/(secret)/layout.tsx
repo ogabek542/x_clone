1010d20c7d4d7710002b5099ce4817a742dbde87
ogabek542 <otaxonovo22222@gmail.com>
2025-11-21 17:53:07 +0500

    added calendar ui from shadcn-ui

--- my-app/app/(secret)/layout.tsx
+++ my-app/app/(secret)/layout.tsx
@@ -6,6 +6,7 @@ import { useConvexAuth } from "convex/react";
 import { Loader } from "@/components/ui/loader";
 import { redirect } from "next/navigation";
 import { Sidebar } from "./components";
+import SearchCommand from "@/components/shared/search-command";
 
 const SecretLayout = ({ children }: ChildProps) => {
   const { isAuthenticated, isLoading } = useConvexAuth();
@@ -23,7 +24,10 @@ const SecretLayout = ({ children }: ChildProps) => {
   return (
     <div className="flex w-full">
       <Sidebar />
-      <main className="flex-1 h-full overflow-y-auto">{children}</main>
+      <main className="flex-1 h-full overflow-y-auto">
+        <SearchCommand />
+        {children}
+      </main>
     </div>
   );
 };
--- my-app/components/shared/search-command.tsx
+++ my-app/components/shared/search-command.tsx
@@ -1,9 +1,75 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useUser } from "@clerk/clerk-react";
+import { useSearch } from "@/hooks/use-search";
+import { useQuery } from "convex/react";
+import { api } from "@/convex/_generated/api";
+import { useRouter } from "next/navigation";
+import { File } from "lucide-react";
+import {
+  CommandDialog,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "../ui/command";
 
 const SearchCommand = () => {
   const { user } = useUser();
-  return <div>SearchCommand</div>;
+  const search = useSearch();
+  const { isOpen, onClose, onOpen, onToggle } = search;
+  const documents = useQuery(api.document.getSearch);
+  const [mounted, setMounted] = useState(false);
+  const router = useRouter();
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  useEffect(() => {
+    const down = (e: KeyboardEvent) => {
+      if (e.key === "k" && (e.metaKey || e.ctrlKey)) {
+        e.preventDefault();
+        onToggle();
+      }
+    };
+
+    document.addEventListener("keydown", down);
+    return () => document.removeEventListener("keydown", down);
+  }, [onToggle]);
+
+  const onSelect = (id: string) => {
+    router.push(`/documents/${id}`);
+    onClose();
+  };
+
+  if (!mounted) return null;
+
+  return (
+    <CommandDialog open={isOpen} onOpenChange={onClose}>
+      <CommandInput placeholder={`Search ${user?.firstName}'s Notion `} />
+      <CommandList>
+        <CommandEmpty>No results found.</CommandEmpty>
+        <CommandGroup heading={"Documents"}>
+          {documents?.map((document) => (
+            <CommandItem
+              key={document._id}
+              value={`${document._id}-${document.title} `}
+              title={document.title}
+              onSelect={onSelect}
+            >
+              {document.icon ? (
+                <p className="mr-2 text-[18px]">{document.icon}</p>
+              ) : (
+                <File className="mr-2 tex" />
+              )}
+              <span>{document.title}</span>
+            </CommandItem>
+          ))}
+        </CommandGroup>
+      </CommandList>
+    </CommandDialog>
+  );
 };
 
 export default SearchCommand;
