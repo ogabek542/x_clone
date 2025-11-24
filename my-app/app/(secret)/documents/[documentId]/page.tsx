f1b19078860d94ee86762c587d433d64f2c153c7
ogabek542 <otaxonovo22222@gmail.com>
2025-11-24 18:30:04 +0500

    added cover url

--- my-app/app/(secret)/documents/[documentId]/page.tsx
+++ my-app/app/(secret)/documents/[documentId]/page.tsx
@@ -1,14 +1,36 @@
 "use client";
 
-import React from "react";
-import { useParams } from "next/navigation";
+import Cover from "@/components/shared/cover";
+// import Toolbar from "@/components/shared/toolbar";
+import { Skeleton } from "@/components/ui/skeleton";
+import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
+import { useMutation, useQuery } from "convex/react";
+import dynamic from "next/dynamic";
+import React, { useMemo } from "react";
+// import "@blocknote/core/style.css";
 
-const DocumentIdPage = () => {
-  const params = useParams<{ documentId: string }>();
-  const documentId = params.documentId as Id<"documents">;
+interface DocumentIdPageProps {
+  params: {
+    documentId: Id<"documents">;
+  };
+}
 
-  return <div className="mt-24">{documentId}</div>;
+const DocumentIdPage = ({ params }: DocumentIdPageProps) => {
+  const document = useQuery(api.document.getDocumentById, {
+    id: params.documentId as Id<"documents">,
+  });
+  const updateFields = useMutation(api.document.updateFields);
+
+  if (document === null) return null;
+
+  return (
+    <div className="pb-40">
+      <Cover url={document?.coverImage} />
+
+      <div className="md:max-w-3xl lg:max-w-4xl mx-auto"></div>
+    </div>
+  );
 };
 
 export default DocumentIdPage;
--- my-app/components/shared/cover.tsx
+++ my-app/components/shared/cover.tsx
@@ -1,12 +1,24 @@
 import React from "react";
+import { cn } from "@/lib/utils";
+import Image from "next/image";
 
 interface CoverProps {
   url?: string;
   preview?: string;
 }
 
-const Cover = () => {
-  return <div></div>;
+const Cover = ({ url, preview }: CoverProps) => {
+  return (
+    <div
+      className={cn(
+        "relative w-full h-[30vh] group",
+        !url && "h-[10vh]",
+        url && "bg-muted"
+      )}
+    >
+      {!!url && <Image src={url} alt="cover" fill className="object-cover" />}
+    </div>
+  );
 };
 
 export default Cover;
